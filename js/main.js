f0ae56540238ec21e54c852738a5ef4c3536977f
somsir95 <somsubhra.moople19@gmail.com>
2024-12-19 22:21:45 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,22 @@
+
+
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    dots:true,
+    autoplay:true,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
